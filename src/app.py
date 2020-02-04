f2318ad8720f58afe63e14c4763dc1e603e9b147
Ilija Vukotic <ivukotic@cern.ch>
2020-02-04 16:50:12 -0600

    test

--- src/app.py
+++ src/app.py
@@ -4,7 +4,8 @@ import dash_html_components as html
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 
 server = app.server
 
