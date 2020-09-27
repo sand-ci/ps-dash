c01e4eb3e9567f5f540c5f3fd5488614f8653ca5
petya-vasileva <petya.tsv.vasileva@gmail.com>
2020-09-27 17:57:35 +0300

    Add callbacks to serve the new pair plots page

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 import dash
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output, State, MATCH, ALL
+from dash.exceptions import PreventUpdate
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
@@ -10,7 +11,8 @@ from model.DataLoader import Updater
 # import view.host_map as host_map
 import view.site_report as site_report
 from view.problematic_pairs import ProblematicPairsPage
-
+from view.pair_plots import PairPlotsPage
+import utils.helpers as hp
 
 # Start a thread which will update the data every hour
 Updater()
@@ -18,14 +20,22 @@ ppage = ProblematicPairsPage()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.BOOTSTRAP]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
 app.layout = html.Div([
-                dcc.Tabs([
+                dcc.Location(id='change-url', refresh=False),
+                html.Div(id='page-content'),
+                dcc.Loading(id="loader", type="default", fullscreen=True,
+                    children=[
+                        html.Div(id='page-content1')
+                    ]
+                ),
+            ])
+
+
+layout_tabs =  dcc.Tabs([
                     dcc.Tab(label='Sites', id='sites-tab', children=[
                         html.Div([
-                            dcc.Location(id='url', refresh=False),
-                            dcc.Store(id='memory-output'),
                             dcc.Interval(
                                     id='interval-component',
                                     interval=1*450, # in milliseconds
@@ -45,32 +55,31 @@ app.layout = html.Div([
                             ]
                     )
                 ])
-            ])
 
 
-
-@app.callback(Output('cards', 'children'),
+'''Build the site summaries as a set of smaller elements wrapped up in a bigger one'''
+@app.callback([Output('cards', 'children')],
               [Input('interval-component', 'n_intervals')],
               [State('cards', 'children')])
-def siteTables(interval, current_elements):
+def showsSiteTables(interval, current_elements):
     elem_list = []
-
     if (interval%3 == 0):
         elem_list.append(dbc.Row([dbc.Col(site_report.createCard(val))
                                   for val in site_report.sites[interval:interval+3]],
                                  id=f"card-{interval}", className='site-card'))
     if current_elements is not None:
-        return current_elements + elem_list
-    return elem_list
+        return [current_elements + elem_list]
+    return [elem_list]
 
+
+'''Get the relevant dataframe based on the type of problem. Page loading is much faster this way'''
 @app.callback([Output('tabs-content', 'children'),
                Output('last-updated', 'children')],
               [Input('tabs-indeces', 'value'),
-               Input('tabs-prob-types', 'value'),
-               Input('update-interval-component', 'n_intervals')])
-def render_problems(idx, problem, intv):
-    print('*** Reload content ***')
+               Input('tabs-prob-types', 'value')])
+def renderProblems(idx, problem):
     ppage = ProblematicPairsPage()
+
     if (problem == 'high_sigma'):
         df = ppage.high_sigma
     elif (problem == 'has_bursts'):
@@ -83,6 +92,57 @@ def render_problems(idx, problem, intv):
                                             html.Div('(Queried period in UTC time)', className='period-times')], className='period-element'
                                          )]
 
+
+'''Store the data from the clicked row in another element'''
+@app.callback(
+        [Output({'type': 'input-src','index': MATCH}, 'value'),
+         Output({'type': 'input-dest','index': MATCH}, 'value'),
+         Output({'type': 'memory-output','index': MATCH}, 'value'),],
+        [Input({'type': 'problem-table','index': MATCH}, 'active_cell'),
+         Input({'type': 'problem-table','index': MATCH}, 'data'),
+         Input({'type': 'problem-table','index': MATCH}, 'page_current'),
+         Input({'type': 'problem-table','index': MATCH}, 'page_size'),
+         Input({'type': 'problem-table','index': MATCH}, 'id')])
+def fillInput(active_cell, data, page, page_size, elem_id):
+    if active_cell is not None:
+        df = pd.DataFrame(data)
+#         active_cell is printing {'row': 0, 'column': 0, 'column_id': 'src_host'}
+        loc = page*page_size+active_cell['row'] if page is not None else active_cell['row']
+        active_row_id = df.loc[loc].to_dict()
+        active_row_id['host_src'] = active_row_id['host_src'] if active_row_id['host_src'] != 'N/A' else active_row_id['src']
+        active_row_id['host_dest'] = active_row_id['host_dest'] if active_row_id['host_dest'] != 'N/A' else active_row_id['dest']
+        active_row_id['idx'] = elem_id['index']
+        return [active_row_id['host_src'], active_row_id['host_dest'], active_row_id]
+    raise PreventUpdate
+
+
+'''Simulate opening of a new page to show the relevant plots'''
+@app.callback([Output('change-url', 'pathname')],
+              [Input({'type': 'plot', 'index': ALL}, 'n_clicks'),
+               Input({'type': 'memory-output','index': ALL}, 'value')])
+def changePath(clicked, stored):
+    idx = None
+    # get the index of the button click and use it to get the data from the corresponding table
+    for i in range(len(clicked)):
+        if clicked[i] > 0:
+            idx = hp.INDECES[i]
+            data = stored[i]
+    if idx is not None:
+        return [f'/plot?idx={data["idx"]}&src_host={data["host_src"]}&src={data["src"]}&dest_host={data["host_dest"]}&dest={data["dest"]}']
+    else: raise PreventUpdate
+
+
+
+# TODO: remove the long sites page and replace it woth a gep map. The the container without the loader can be removed as well
+'''Show different layouts depending on the URL'''
+@app.callback([Output('page-content', 'children'), Output('page-content1', 'children'),],[Input('change-url', 'pathname'), Input('change-url', 'href')])
+def displayPage(pathname, url):
+    if pathname == '/':
+        return [layout_tabs, None]
+    elif pathname.startswith('/plot'):
+        path = pathname if len(pathname) > 5 else url
+        o = PairPlotsPage(path)
+        return [None, o.createLayout()]
 
 
 app.run_server(debug=False, port=8050, host='0.0.0.0')
