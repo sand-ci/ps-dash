1c2ab3a1727bcbe514c4ba2aeeb24c14b77c4713
petya-vasileva <petya.tsv.vasileva@gmail.com>
2020-10-08 13:41:17 +0300

    Fill dropdowns with data depending on previos selection

--- src/app.py
+++ src/app.py
@@ -136,8 +136,75 @@ def changePath(clicked, stored):
     else: raise PreventUpdate
 
 
+@app.callback([Output("src-dropdown", "options"),
+               Output("dest-dropdown", "options"),
+               Output("store-dropdown", "data"),
+               Output("total-pairs", "children"),
+               Output("total-srcs", "children"),
+               Output("total-dests", "children")],
+              [Input("idx-dropdown", "value"),
+               Input("src-dropdown", "value"),
+               Input("dest-dropdown", "value")],
+               State("store-dropdown", "data"))
+def fill_dropdowns(idx, src, dest, stored):
+    idxChanged = False
+    if stored is not None:
+        if idx != stored['idx']:
+            idxChanged = True
+    srcPresent = ''
+    ip_src, host_src, ip_dest, host_dest = None, None, None, None
 
-# TODO: remove the long sites page and replace it woth a gep map. The the container without the loader can be removed as well
+    if idx is not None:
+        # This is the case when the values come from the URL
+        if (src is not None) and (dest is not None):
+            s = src.split(': ')
+            d = dest.split(': ')
+            ip_src = s[0]
+            ip_dest = d[0]
+            host_src = s[1]
+            host_dest = d[1]
+            slist = gdl.all_tested_pairs[gdl.all_tested_pairs['idx'] == idx]['source'].unique()
+            dlist = gdl.all_tested_pairs[(gdl.all_tested_pairs['source'] == src) & (gdl.all_tested_pairs['idx'] == idx)]['destination'].unique()
+        # Get all values for the selected index
+        if (dest is None and src is None) or (idxChanged):
+            slist = gdl.all_tested_pairs[gdl.all_tested_pairs['idx'] == idx]['source'].unique()
+            dlist = gdl.all_tested_pairs[gdl.all_tested_pairs['idx'] == idx]['destination'].unique()
+        # Get all destinations for the selected source and index
+        elif (src is not None) or (src != stored['src']):
+            srcPresent = f' for selected source'
+            ip_src = gdl.all_tested_pairs[(gdl.all_tested_pairs['source'] == src)]['src'].values[0]
+            ip_dest = gdl.all_tested_pairs[(gdl.all_tested_pairs['destination'] == dest)]['dest'].values[0] if dest is not None else None
+            host_src = gdl.all_tested_pairs[(gdl.all_tested_pairs['source'] == src)]['host_src'].values[0]
+            host_dest = gdl.all_tested_pairs[(gdl.all_tested_pairs['destination'] == dest)]['host_dest'].values[0] if dest is not None else None
+            slist = gdl.all_tested_pairs[gdl.all_tested_pairs['idx'] == idx]['source'].unique()
+            dlist = gdl.all_tested_pairs[(gdl.all_tested_pairs['source'] == src) & (gdl.all_tested_pairs['idx'] == idx)]['destination'].unique()
+
+        data = {'idx': idx, 'source': src, 'destination': dest, 'src': ip_src, 'dest': ip_dest, 'host_src': host_src, 'host_dest': host_dest}
+
+        return [[{'label':v, 'value':v} for v in slist],
+                [{'label':v, 'value':v} for v in dlist],
+                data,
+                f"Total number of pairs for selected index: {len(gdl.all_tested_pairs[gdl.all_tested_pairs['idx'] == idx])}",
+                f"Number of sources for selected index: {len(slist)}",
+                f"Number of destinations{srcPresent}: {len(dlist)}"]
+
+    raise PreventUpdate
+
+'''Show different layouts depending on the URL'''
+@app.callback([Output("idx-dropdown", "value"),
+               Output("src-dropdown", "value"),
+               Output("dest-dropdown", "value")],
+              [Input('change-url', 'href')])
+def update_dropdowns_from_url(url_data):
+    parsed = urlparse.urlparse(url_data)
+    data = parse_qs(parsed.query)
+
+    if bool(data):
+        return [data['idx'][0], f"{data['src_host'][0]}: {data['src'][0]}", f"{data['dest_host'][0]}: {data['dest'][0]}"]
+    raise PreventUpdate
+
+
+# TODO: remove the long sites page and replace it woth a geo map. Then the container without the loader can be removed as well
 '''Show different layouts depending on the URL'''
 @app.callback([Output('page-content', 'children'), Output('page-content1', 'children'),],[Input('change-url', 'pathname'), Input('change-url', 'href')])
 def displayPage(pathname, url):
