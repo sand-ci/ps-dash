fe6fdfdf55271f1458e809c84f9a9713c44a8b42
maxymnaumchyk <maxymnaumchyk@gmail.com>
2023-11-30 21:45:42 +0200

    Put back 60 days preloading for packetloss

--- src/pages/packet-loss-ml.py
+++ src/pages/packet-loss-ml.py
@@ -37,7 +37,7 @@ dash.register_page(
 )
 
 def layout(**other_unknown_query_strings):
-    now = hp.defaultTimeRange(days=90, datesOnly=True)
+    now = hp.defaultTimeRange(days=60, datesOnly=True)
 
     #Packet loss alarms page
     return \
@@ -275,11 +275,11 @@ def update_output(start_date, end_date, sensitivity, sitesState):
     if start_date and end_date:
         start_date, end_date = [f'{start_date}T00:01:00.000Z', f'{end_date}T00:01:00.000Z']
     else:
-        start_date, end_date = hp.defaultTimeRange(days=90, datesOnly=True)
+        start_date, end_date = hp.defaultTimeRange(days=60, datesOnly=True)
         start_date, end_date = [f'{start_date}T00:01:00.000Z', f'{end_date}T00:01:00.000Z']
 
     # check if the date range is default
-    start_date_check, end_date_check = hp.defaultTimeRange(days=90, datesOnly=True)
+    start_date_check, end_date_check = hp.defaultTimeRange(days=60, datesOnly=True)
     start_date_check, end_date_check = [f'{start_date_check}T00:01:00.000Z', f'{end_date_check}T00:01:00.000Z']
 
     # query for the dataset
