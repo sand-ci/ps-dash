83a719349b26619bad578d3e7081606524cf2454
Yana Holoborodko <yanaholoborodko@yana-macbook.dyndns.cern.ch>
2025-03-18 12:58:04 +0100

    Remove alarm_button from front page for hosts_not_found

--- src/pages/home.py
+++ src/pages/home.py
@@ -711,10 +711,8 @@ def generate_tables(frame, unpacked, event, alarmsInst):
     ids = unpacked['id'].values
     dfr = frame[frame.index.isin(ids)]
     dfr = alarmsInst.formatDfValues(dfr, event)
-    # try:
-    #     print(f"###################\n{dfr}")
-    # except Exception as err:
-    #     print(err)
+    if event == 'hosts not found':
+        dfr.drop(columns=['alarm_button'], inplace=True)
     dfr.sort_values('to', ascending=False, inplace=True)
     print('Home page,', event, "Number of alarms:", len(dfr))
     try:
