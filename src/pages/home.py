0a6c7be9fd086c7088fa2a4739ff3bd6ce8a4f8c
Petya Vasileva <petya.tsv.vasileva@gmail.com>
2025-05-05 14:15:06 +0300

    Update default time range to 2 days and adjust date handling in update_output function

--- src/pages/home.py
+++ src/pages/home.py
@@ -373,7 +373,7 @@ alarmsInst = Alarms()
 
 
 def layout(**other_unknown_query_strings):
-    dateFrom, dateTo = hp.defaultTimeRange(1)
+    dateFrom, dateTo = hp.defaultTimeRange(days=2)
     now = hp.defaultTimeRange(days=2, datesOnly=True)
     alarmCnt = pq.readFile('parquet/alarmsGrouped.parquet')
     statusTable, sitesDf = generate_status_table(alarmCnt)
@@ -522,8 +522,11 @@ def update_output(n_clicks, start_date, end_date, sites, all, events, allevents,
 
     if not ctx.triggered or ctx.triggered[0]['prop_id'].split('.')[0] == 'search-button':
         if start_date and end_date:
-            start_date, end_date = [f'{start_date}T00:01:00.000Z', f'{end_date}T23:59:59.000Z']
-        else: start_date, end_date = hp.defaultTimeRange(2)
+            current_hour = pd.Timestamp.now().hour
+            start_date = pd.Timestamp(start_date).replace(hour=current_hour, minute=0, second=0, microsecond=0).isoformat()
+            end_date = pd.Timestamp(end_date).replace(hour=current_hour, minute=0, second=0, microsecond=0).isoformat()
+        else: start_date, end_date = hp.defaultTimeRange(days=2)
+
         alarmsInst = Alarms()
         frames, pivotFrames = alarmsInst.loadData(start_date, end_date)
 
