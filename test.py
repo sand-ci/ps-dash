bfb1d608678bd2652d789019a4d678246d5b4846
Yana <71765009+hooloobooroodkoo@users.noreply.github.com>
2025-07-03 17:02:43 +0200

    Delete test.py

--- test.py
+++
@@ -1,3 +0,0 @@
-from elasticsearch import Elasticsearch
-es = Elasticsearch('https://localhost:9200', verify_certs=True, basic_auth=('yana', '5ysfuGptvh'), max_retries=20)
-print('Success' if es.ping()==True else 'Fail')
